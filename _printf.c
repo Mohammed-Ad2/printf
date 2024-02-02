ea2ab1011b59ed653e26bcea21818aee316ba6fc
Mohammed <simoaddame17@gmail.com>
2024-02-02 01:54:29 -0800

    printf

---
+++ _printf.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _printf - Prints formatted output to the standard output stream.
+ * @format: A pointer to a character string containing conversion specifiers.
+ *          The format string can include zero or more directives.
+ *
+ * Return: The number of characters printed (excluding the null byte).
+ */
+int _printf(const char *format, ...)
+{
+	fmt f[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'d', print_int},
+		{'i', print_int},
+		{'\0', NULL},
+	};
+
+	int count = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	count = help_printf(format, &args, f);
+
+	va_end(args);
+	return (count);
+}
